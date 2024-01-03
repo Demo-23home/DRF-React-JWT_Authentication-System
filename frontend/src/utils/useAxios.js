4e70289feedb440728597e32caf8cf746c76af7c
demo-23home <zeyadslama23@gmail.com>
2024-01-03 13:23:46 +0200

    Creating(UseAxios,PrivateRoute)

--- frontend/src/utils/useAxios.js
+++ frontend/src/utils/useAxios.js
@@ -9,4 +9,34 @@ const baseUrl = "http://localhost:8000/api";
 
 const useAxios = () => {
     const {authTokens, setUser, setAuthTokens} = useContext(AuthContext)
+    
+    const axiosInstance = axios.create({
+        baseURL,
+        headers: {Authorization: `Bearer ${authTokens?.access}`}
+    }).access
+
+    axiosInstance.interceptors.request.use(async req => {
+        const user = jwtDecode(authTokens.access)
+        const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1
+
+        if (isExpired) return req
+
+        const response = await axios.post(`${baseUrl}/token/refresh`, {
+            refresh: authTokens.refresh
+        })
+
+        localStorage.setItem("authToken", JSON.stringify(response.data))
+        
+        setAuthTokens(response.data)
+        setUser(jwtDecode(response.data.access))
+
+
+        req.headers.Authorization = `${response.data.access}`
+        return req
+    })
+
+    return axiosInstance
 }
+
+
+export default useAxios
